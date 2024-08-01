315507a6761d8ab839e034aedc80841ac5d6719d
saepudinasep <saepudinasep2001@gmail.com>
2024-08-01 12:10:41 +0700

    [UPDATE] Merubah struktur tabel user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,11 +23,15 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        // Daftar role
+        $roles = ['pelanggan', 'admin'];
+
         return [
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
+            'role' => $this->faker->randomElement($roles), // Pilih role secara acak
             'remember_token' => Str::random(10),
         ];
     }
